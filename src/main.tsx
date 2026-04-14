01dddb21792c26bd0e3071fb959cab0faa623c81
KyawWannaBL <mgkyawwanna@gmail.com>
2026-04-14 11:24:45 +0630

    Fix broken main.tsx overwritten with shell command

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
+import "./index.css";
 import "./styles/enterprise-skin.css";
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(<App />);
